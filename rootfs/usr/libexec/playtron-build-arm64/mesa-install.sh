dd6c0ea88260f702b7ceeeee09303099a131f20b
Ericky Dos Santos <ericky@playtron.one>
2026-07-31 17:31:07 -0400

    feat: update mesa version to 26.2, fix glymur udev governor rule, and grub config during customize

--- rootfs/usr/libexec/playtron-build-arm64/mesa-install.sh
+++ rootfs/usr/libexec/playtron-build-arm64/mesa-install.sh
@@ -2,10 +2,12 @@
 
 set -e -x
 
-dnf install -y \
-  mesa-dri-drivers \
-  mesa-filesystem \
-  mesa-libEGL \
-  mesa-libgbm \
-  mesa-libGL \
-  mesa-vulkan-drivers
+MESA_VERSION=26.2.0-0.8.git79e16e7.fc43
+
+dnf install -y --enablerepo=mesa-git \
+  "mesa-dri-drivers-${MESA_VERSION}" \
+  "mesa-filesystem-${MESA_VERSION}" \
+  "mesa-libEGL-${MESA_VERSION}" \
+  "mesa-libgbm-${MESA_VERSION}" \
+  "mesa-libGL-${MESA_VERSION}" \
+  "mesa-vulkan-drivers-${MESA_VERSION}"
